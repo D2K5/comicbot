cade7d7e7edcd6b5f39344bf467b04608a8a1216
kei <kei.trei.a52@gmail.com>
2020-04-25 11:16:40 +0100

    improved formatting and fixed indentation

--- CartoonStripBot.java
+++ CartoonStripBot.java
@@ -27,35 +27,58 @@ import org.jibble.pircbot.*;
  * @author Paul Mutton http://www.jibble.org/comicbot/
  */
 public class CartoonStripBot extends PircBot{
-
     public static final int MAX_QUOTES = 9;
 
     public CartoonStripBot(File outputDirectory, String helpString, String channel, String triggers) {
         _outputDirectory = outputDirectory;
         _helpString = helpString;
         _channel = channel;
-	_triggers = triggers;
+        _triggers = triggers;
     }
- 
+
     public void onMessage(String channel, String sender, String login, String hostname, String message) {
         String lowMsg = message.trim().toLowerCase();
         processMessage(sender, message);
     }
-    
+
     public void onAction(String sender, String login, String hostname, String target, String action) {
         processMessage(sender, "Me " + action);
     }
  
     public void processMessage(String sender, String message) {
+	Boolean can_add = true;
+	Boolean found = false;
+
+        //strip urls
+        message = message.replaceAll("((http|ftp|https):\\/\\/)?[\\w\\-_]+(\\.[\\w\\-_]+)+([\\w\\-\\.,@?^=%&amp;:/~\\+#]*[\\w\\-\\@?^=%&amp;/~\\+#])?", "");
+
+        //strip html
+        message = message.replaceAll("\\<.*?>", "");
+
+        //strip custom emoticons
+        message = message.replaceAll(":[^:\\s]*(?:::[^:\\s]*)*:", "");
+
+        //strip all non-alphanumeric/language/etc characters (remove emoji)
+        message = message.replaceAll("[^\\p{L}\\p{M}\\p{N}\\p{P}\\p{Z}\\p{Cf}\\p{Cs}\\s]","");
+
+	//trim spaces
         message = message.trim();
+
+        //contains an upload?
+        if (message.contains("ACTION uploaded") || message.contains("Me uploaded")) {
+            System.out.println("contains an upload, ignoring");
+            can_add = false;
+        }
+
+        //is empty?
+        if(message == null || message == " " || message == "" || message.isEmpty()) {
+            System.out.println("no message, ignoring");
+            can_add = false;
+        }
+
         String lowMsg = message.toLowerCase();
-	        
-
-
         String triggers = _triggers;
         String[] split = triggers.split(";");
-        Boolean found = false;
-	Boolean can_add = true;        
 
         for (int i = 0; i < split.length; i++)
         {
@@ -65,12 +88,12 @@ public class CartoonStripBot extends PircBot{
                 break;
             }
         }
-        
-        if (_quotes.size() == MAX_QUOTES && found) {
+
+        if (_quotes.size() == MAX_QUOTES && found && can_add) {
             // add the current quote, so that it's always the last thing said
             _quotes.add(message);
             _senders.add(sender);
-            
+
             // Let's make a cartoon!
             String[] texts = new String[MAX_QUOTES];
             String[] nicks = new String[MAX_QUOTES];
@@ -84,39 +107,25 @@ public class CartoonStripBot extends PircBot{
             }
             try {
                 String result = ComicTest.createCartoonStrip(_outputDirectory, texts, nicks);
-		if (result != "false") {
-                        sendMessage(_channel, _helpString + result);
-		}
+                if (result != "false") {
+                    sendMessage(_channel, _helpString + result);
+                }
             }
             catch (IOException e) {
                 //sendMessage(_channel, "Urgh, I'm crap cos I just did this: " + e);
             }
             _quotes.clear();
             _senders.clear();
-        }
-        else {
-	   message = message.replaceAll("((http|ftp|https):\\/\\/)?[\\w\\-_]+(\\.[\\w\\-_]+)+([\\w\\-\\.,@?^=%&amp;:/~\\+#]*[\\w\\-\\@?^=%&amp;/~\\+#])?", "");
-	   message = message.replaceAll("\\<.*?>", "");
-	   message = message.replaceAll(":[^:\\s]*(?:::[^:\\s]*)*:", "");
-
-	   if(message == null || message == " " || message == "" || message.isEmpty()) {
-		System.out.println("no message, ignoring");
-		can_add = false;
-	   }
-
-	   if (message.contains("uploaded an image:")) {
-		can_add = false;
-	   }
-
-	   if (can_add){
-		System.out.println("Quote Added: " + message);
-            	_quotes.add(message);
-            	_senders.add(sender);
-            	if (_quotes.size() > MAX_QUOTES) {
-             	   _quotes.removeFirst();
-             	   _senders.removeFirst();
-            	}
-	   }
+        } else {
+            if (can_add){
+                System.out.println("Quote Added: " + message);
+                _quotes.add(message);
+                _senders.add(sender);
+                if (_quotes.size() > MAX_QUOTES) {
+                    _quotes.removeFirst();
+                    _senders.removeFirst();
+                }
+            }
         }
     }
           
@@ -129,7 +138,7 @@ public class CartoonStripBot extends PircBot{
             System.exit(1);
         }
         String channel = p.getProperty("channel");
-	String triggers = p.getProperty("triggers");
+        String triggers = p.getProperty("triggers");
         CartoonStripBot bot = new CartoonStripBot(outputDirectory, p.getProperty("helpString"), channel, triggers);
         bot.setVerbose(true);
         bot.setName(p.getProperty("nick"));
@@ -144,5 +153,4 @@ public class CartoonStripBot extends PircBot{
     private String _triggers;
     private LinkedList _quotes = new LinkedList();
     private LinkedList _senders = new LinkedList();
-    
 }
